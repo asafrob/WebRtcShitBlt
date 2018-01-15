c584dd77ba498f9d151419cd8970f766b615d2cb
asafrob <asafrob@gmail.com>
2018-01-15 16:49:05 +0200

    remove global

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # WebRtcShitBlt
+
+this library is based on ES6, so if u use a web browser that does not support it something like Bubble is required
+
+
--- WebRtcShitBlt.js
+++ WebRtcShitBlt.js
@@ -1,38 +1,34 @@
-
-// general WebRTC initializations
-
-var sbVidContext;
-var sbVideoSrc;
-var myImage;
 class WebRtcSB
 {
-    constructor(uniqueName){
+    constructor(constraints = {video: true, audio: true},
+                uniqueName = 'sbUniqueName',
+                imagePath = 'sb.png')
+    {
+        this._constraints = constraints;
         this._uniqueNamme = uniqueName;
         this._createHiddenVideoElemment();
         this._createHiddenCanvas();
-        myImage = new Image();
-        myImage.src = 'sb.png';
+        this._myImage = new Image();
+        this._myImage.src = imagePath;
     }
 
     // returns a promise resolving to a MediaStream
-    sbStartCapture(constraints = {video: true, audio: false})
+    sbStartCapture()
     {
         return Promise.resolve()
             .then(()=>{
-                return navigator.mediaDevices.getUserMedia(constraints);
+                return navigator.mediaDevices.getUserMedia(this._constraints);
             })
             .then((stream) => {
-                //e.src = window.URL.createObjectURL(stream);
-                //e.muted = true;
                 this._hiddenVideoElement.srcObject = stream;
-                sbVidContext = this._hiddenCanvasElement.getContext("2d"); // need to deal with this if we want to be able to run 2 instances
-                requestAnimationFrame(_sendImageToCanvas);
+                this._sbVidContext = this._hiddenCanvasElement.getContext("2d"); // need to deal with this if we want to be able to run 2 instances
+                requestAnimationFrame(this._sendImageToCanvas.bind(this));
                 return Promise.resolve(this._hiddenCanvasElement.captureStream());
             })
-
     }
 
-    _createHiddenVideoElemment(){
+    _createHiddenVideoElemment()
+    {
         this._hiddenVideoElement = document.createElement('video');
         this._hiddenVideoElement.style.display = 'none';
         this._hiddenVideoElement.setAttribute('id', this._uniqueNamme+'_SBSrc');
@@ -40,10 +36,10 @@ class WebRtcSB
         this._hiddenVideoElement.setAttribute("width", '640px');
         this._hiddenVideoElement.setAttribute("height", '480px');
         document.body.appendChild(this._hiddenVideoElement);
-        sbVideoSrc = this._hiddenVideoElement;
     }
 
-    _createHiddenCanvas(){
+    _createHiddenCanvas()
+    {
         this._hiddenCanvasElement = document.createElement('canvas');
         this._hiddenCanvasElement.style.display = 'none';
         this._hiddenCanvasElement.setAttribute('id', this._uniqueNamme+'_SBCanvas');
@@ -51,12 +47,13 @@ class WebRtcSB
         this._hiddenCanvasElement.setAttribute("height", '480px');
         document.body.appendChild(this._hiddenCanvasElement);
     }
+    
+    _sendImageToCanvas()
+    {
+        this._sbVidContext.drawImage(this._hiddenVideoElement, 0, 0, this._hiddenVideoElement.width, this._hiddenVideoElement.height);
+        this._sbVidContext.drawImage(this._myImage, 0, 0, 50, 50);
+        requestAnimationFrame(this._sendImageToCanvas.bind(this));
+    }
 }
 
-function _sendImageToCanvas()
-{
-    sbVidContext.drawImage(sbVideoSrc, 0, 0, sbVideoSrc.width, sbVideoSrc.height);
-    sbVidContext.drawImage(myImage, 0,0, 50,50);
 
-    requestAnimationFrame(_sendImageToCanvas);
-}
